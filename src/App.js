dcbc075bbdb5d9b3735633b666f1cf4522deb29e
sarath-sms <sarathmca2016@gmail.com>
2021-06-14 00:51:15 +0530

    converted functional app component to class comp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
+import React, {Component, component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      string: 'just a value'
+    }
+  }
+  render() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {this.state.string}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <button onClick={() => this.setState({string: 'good job'})}>
           Learn React
-        </a>
+        </button>
       </header>
     </div>
   );
-}
+}}
 
 export default App;
